3fcf86cb05acf9337b0bf60fa088f4fa5d8c9ba3
daniel692a <daniel62armas@gmail.com>
2022-01-27 20:56:05 -0600

    Knowing more basic data types in C++

---
+++ bools.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    bool accepted = true;
+    bool rejected = false;
+    std::cout<<accepted<<" "<<rejected;
+    return 0;
+}
---
+++ convert_type.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    float payment = 90.10;
+    int charged = (int)payment;
+    std::cout<<payment<<"\n";
+    std::cout<<charged;
+    int x = 32;
+    float y = x;
+    std::cout<<y;
+    return 0;
+}
