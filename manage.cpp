6a2f285fa68b37ab2e5cfe1221efd647743efe27
daniel692a <daniel62armas@gmail.com>
2022-03-16 22:51:35 -0600

    Improve employee manager

--- manage.cpp
+++ manage.cpp
@@ -6,24 +6,35 @@ using namespace std;
 
 enum Type {A =300, B=350, C=500, D=0};
 
+class Employee{
+	public:
+		int id;
+		float average_salary;
+		Type type;
+		string name, last_name;
+		static int date_check; //Será el mismo valor para todos los objetos creados a partir de esta clase, es como un valor compartido
+
+		float calculate_salary();
+	private:
+};
 int main(){
-	int id;
+
+	Employee employee;
+
 	vector<string> months{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 	vector<float> wage(12);
 
-	float sum_s = 0.0, average_salary=0.0;
-	string name, last_name;
-	Type type;
+	float sum_s = 0.0;
 
 	cout<<"Please input your personal data\n";
 	cout<<"Id: ";
-	cin>>id;
+	cin>>employee.id;
 
 	cout<<"Name: ";
-	cin>>name;
+	cin>>employee.name;
 
 	cout<<"Last name: ";
-	cin>>last_name;
+	cin>>employee.last_name;
 
 	for(int i=0; i<12; i++){
 		cout<<"Input salary of "<<months[i]<<": ";
@@ -34,10 +45,10 @@ int main(){
 		sum_s+=wage[i];
 	}
 
-	average_salary = sum_s/12.0;
+	employee.average_salary = sum_s/12.0;
 
-	cout<<"Welcome "<<name<<" "<<last_name<<"\n";
-	cout<<"The average salary of "<<name<<" "<<last_name<<" is "<<average_salary;
+	cout<<"Welcome "<<employee.name<<" "<<employee.last_name<<"\n";
+	cout<<"The average salary of "<<employee.name<<" "<<employee.last_name<<" is "<<employee.average_salary;
 
 	char type_employee;
 	cout<<"\n\nPlease input your type of employee: ";
@@ -45,18 +56,23 @@ int main(){
 
 	switch (type_employee){
 	case 'A':
-		type = A;
+		employee.type = A;
 		break;
 	case 'B':
-		type = B;
+		employee.type = B;
 		break;
 	case 'C':
-		type = C;
+		employee.type = C;
 		break;
 	default:
-		type = D;
+		employee.type = D;
 		break;
 	}
 
 	return 0;
+}
+
+//Estamos implementando y no usando
+float Employee::calculate_salary(){
+	return average_salary + type;
 }
